f6d249f073149763f1f1f8188da37215de68a25d
belehx <161518280+belehx@users.noreply.github.com>
2024-02-27 21:32:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("digite um número: ");
+            a = int.Parse(Console.ReadLine());
+
+            if (a%2 == 0)
+            {
+                Console.WriteLine("o número digitado é par!");
+            }
+            else
+            {
+                Console.WriteLine("o número digitado é ímpar!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
